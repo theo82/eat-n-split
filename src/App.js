102b3f54bb0f6bdd282dc391eb1b9359cb1f430e
theo82 <theoaristi53@gmail.com>
2026-03-20 15:33:07 +0200

    Forms

--- src/App.js
+++ src/App.js
@@ -24,7 +24,10 @@ export default function App() {
     <div className='app'>
       <div className='sidebar'>
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   )
 }
@@ -67,5 +70,47 @@ function Friend({ friend }) {
 
       <button className='button'>Select</button>
     </li>
+  )
+}
+
+function Button({ children }) {
+  return <button className='button'>{children}</button>
+}
+
+function FormAddFriend() {
+  return (
+    <form className='form-add-friend'>
+      <label>👯Friend Name</label>
+      <input type='text' />
+
+      <label>🎆Image Url</label>
+      <input type='text' />
+
+      <Button>Add</Button>
+    </form>
+  )
+}
+
+function FormSplitBill() {
+  return (
+    <form className='form-split-bill'>
+      <h2>Split a bill with X</h2>
+
+      <label>💰Bill Value</label>
+      <input type='text' />
+
+      <label>🧍‍♂️Your Expense</label>
+      <input type='text' />
+
+      <label>👬X's expense</label>
+      <input type='text' disabled />
+
+      <label> 🤑 Who is paying the bill</label>
+      <select>
+        <option value='user'>You</option>
+        <option value='friend'>X</option>
+      </select>
+      <Button>Add</Button>
+    </form>
   )
 }
