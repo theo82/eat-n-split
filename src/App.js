36a951163b1b13367eb76519f098a6d89255b738
theo82 <theoaristi53@gmail.com>
2026-03-29 19:52:41 +0300

    Splitting a Bill

--- src/App.js
+++ src/App.js
@@ -48,6 +48,17 @@ export default function App() {
     setSelectedFriend((cur) => (cur?.id === friend.id ? null : friend))
   }
 
+  function handleSplitBill(value) {
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend,
+      ),
+    )
+
+    setSelectedFriend(null)
+  }
   return (
     <div className='app'>
       <div className='sidebar'>
@@ -63,7 +74,12 @@ export default function App() {
           {showAddFriend ? 'Close' : 'Add Friend'}
         </Button>
       </div>
-      {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} />}
+      {selectedFriend && (
+        <FormSplitBill
+          selectedFriend={selectedFriend}
+          onSplitBill={handleSplitBill}
+        />
+      )}
     </div>
   )
 }
@@ -156,14 +172,21 @@ function FormAddFriend({ onAddFriend }) {
   )
 }
 
-function FormSplitBill({ selectedFriend }) {
+function FormSplitBill({ selectedFriend, onSplitBill }) {
   const [bill, setBill] = useState('')
   const [paidByUser, setPaidByUser] = useState('')
   const paidByFriend = bill ? bill - paidByUser : ''
   const [whoIsPaying, setWhoIsPaying] = useState('user')
 
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    if (!bill || !paidByUser) return
+    onSplitBill(whoIsPaying === 'user' ? paidByFriend : -paidByUser)
+  }
+
   return (
-    <form className='form-split-bill'>
+    <form className='form-split-bill' onSubmit={handleSubmit}>
       <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>💰Bill Value</label>
@@ -195,7 +218,7 @@ function FormSplitBill({ selectedFriend }) {
         <option value='user'>You</option>
         <option value='friend'>{selectedFriend.name}</option>
       </select>
-      <Button>Add</Button>
+      <Button>Split Bill</Button>
     </form>
   )
 }
